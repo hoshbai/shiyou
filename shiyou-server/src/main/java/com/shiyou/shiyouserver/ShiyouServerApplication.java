4c2c93fef98f0821a41214022800380611eafaa6
hoshbai <xinjixuluo@mail.hsu.edu.cn>
2025-08-12 21:16:35 +0800

    啦啦啦

--- shiyou-server/src/main/java/com/shiyou/shiyouserver/ShiyouServerApplication.java
+++ shiyou-server/src/main/java/com/shiyou/shiyouserver/ShiyouServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShiyouServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ShiyouServerApplication.class, args);
     }
