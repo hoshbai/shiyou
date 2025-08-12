79aa9dc308586b976a617e870bef097a67601820
hoshbai <xinjixuluo@mail.hsu.edu.cn>
2025-08-12 21:09:19 +0800

    测试

--- shiyou-server/src/main/java/com/shiyou/shiyouserver/ShiyouServerApplication.java
+++ shiyou-server/src/main/java/com/shiyou/shiyouserver/ShiyouServerApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShiyouServerApplication {
-    // 测试一下能不能上传到github
-    // 测试一下能不能上传到gitee
+    // 测试一下能不能上传到github,明天开始书写代码
     public static void main(String[] args) {
         SpringApplication.run(ShiyouServerApplication.class, args);
     }
