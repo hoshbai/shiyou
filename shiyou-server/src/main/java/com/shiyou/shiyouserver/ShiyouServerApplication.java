e4c4c8e2e5402f1c31e2fbe933b4d5f4d535db8f
hoshbai <xinjixuluo@mail.hsu.edu.cn>
2025-08-12 20:51:12 +0800

    🚀 Initial commit: Clean project structure

---
+++ shiyou-server/src/main/java/com/shiyou/shiyouserver/ShiyouServerApplication.java
@@ -0,0 +1,13 @@
+package com.shiyou.shiyouserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiyouServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiyouServerApplication.class, args);
+    }
+
+}
---
+++ shiyou-server/src/test/java/com/shiyou/shiyouserver/ShiyouServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiyou.shiyouserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiyouServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
